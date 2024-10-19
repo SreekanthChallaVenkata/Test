1abd9924d39cd6ed8687f399cf7708d33aeb5755
sreekanthchallavenkata <sreekanth.challa37@gmail.com>
2024-10-19 17:50:26 +0530

    first commit

---
+++ Downloads/demo/demo/src/main/java/com/example/demo/ControllerClass.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerClass
+{
+
+    @GetMapping("/sayHellow")
+    public String sayHellow()
+    {
+        return "Om namah shivaya";
+    }
+}
