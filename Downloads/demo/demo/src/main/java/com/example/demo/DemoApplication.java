5de2599d33a65b37c9a85cffaab93ab5ed7b9dd0
sreekanthchallavenkata <sreekanth.challa37@gmail.com>
2024-10-31 23:00:03 +0530

    updated changes

--- Downloads/demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Downloads/demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
         System.out.println("sreekanth here");
-		System.out.println("developer1 change");
-		System.out.println("developer2 change");
+		System.out.println("developer1 has been change");
+		System.out.println("developer2 has been change");
 		System.out.println("developer3 change");
 	}
 }
